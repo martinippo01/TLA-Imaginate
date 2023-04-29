3ca5c832ac19dc4d60f041cc8ae875491a9db0da
Marco4444 <mscilipoti@itba.edu.ar>
2023-04-29 15:41:46 -0300

    updated tests and added debugging for grammar actions

--- src/frontend/syntactic-analysis/bison-actions.c
+++ src/frontend/syntactic-analysis/bison-actions.c
@@ -23,127 +23,129 @@ void yyerror(const char * string) {
 }
 
 int AssignmentsGrammarAction(const int assignment, const int assignments) {
-    
+    LogDebug("AssignmentsGrammarAction: assignment = %d, assignments = %d", assignment, assignments);
     return 0;
 }
 
 int AssignmentGrammarAction(const int assignment, const int assignments) {
-	return 0;
+    LogDebug("AssignmentGrammarAction: assignment = %d, assignments = %d", assignment, assignments);
+    return 0;
 }
+
 int EmptyAssignmentsGrammarAction() {
-    
+    LogDebug("EmptyAssignmentsGrammarAction");
     return 0;
 }
 
 int VariableIdentifierGrammarAction(const int variable) {
-	return 0;
+    LogDebug("VariableIdentifierGrammarAction: variable = %d", variable);
+    return 0;
 }
 
 int ValueStringGrammarAction(const int sval) {
-    
+    LogDebug("ValueStringGrammarAction: sval = %d", sval);
     return 0;
 }
 
 int ValueIntegerGrammarAction(const int ival) {
-    
+    LogDebug("ValueIntegerGrammarAction: ival = %d", ival);
     return 0;
 }
 
 int DefinitionsGrammarAction(const int definition, const int definitions) {
-    
+    LogDebug("DefinitionsGrammarAction: definition = %d, definitions = %d", definition, definitions);
     return 0;
 }
 
 int EmptyDefinitionsGrammarAction() {
-    
+    LogDebug("EmptyDefinitionsGrammarAction");
     return 0;
 }
 
 int DefinitionGrammarAction(const int variableIdentifier, const int methodChain) {
-    
+    LogDebug("DefinitionGrammarAction: variableIdentifier = %d, methodChain = %d", variableIdentifier, methodChain);
     return 0;
 }
 
 int EmptyParamsGrammarAction() {
-    
+    LogDebug("EmptyParamsGrammarAction");
     return 0;
 }
 
 int ImaginateGrammarAction(const int focus, const int methodChain, const int render) {
-    
+    LogDebug("ImaginateGrammarAction: focus = %d, methodChain = %d, render = %d", focus, methodChain, render);
     return 0;
 }
 
 int FocusAddGrammarAction(const int paramsBlock) {
-    
+    LogDebug("FocusAddGrammarAction: paramsBlock = %d", paramsBlock);
     return 0;
 }
 
 int FocusForEachGrammarAction(const int paramsBlock) {
-    
+    LogDebug("FocusForEachGrammarAction: paramsBlock = %d", paramsBlock);
     return 0;
 }
 
 int MethodChainGrammarAction(const int method, const int methodChain) {
-    
+    LogDebug("MethodChainGrammarAction: method = %d, methodChain = %d", method, methodChain);
     return 0;
 }
 
 int EmptyMethodChainGrammarAction() {
-    
+    LogDebug("EmptyMethodChainGrammarAction");
     return 0;
 }
 
 int MethodGrammarAction(const int optional, const int methodIdentifier, const int paramsBlock) {
-    
+    LogDebug("MethodGrammarAction: optional = %d, methodIdentifier = %d, paramsBlock = %d", optional, methodIdentifier, paramsBlock);
     return 0;
 }
 
 int ParamsBlockGrammarAction(const int params) {
-    
+    LogDebug("ParamsBlockGrammarAction: params = %d", params);
     return 0;
 }
 
 int OptionalQuestionSignGrammarAction() {
-    
+    LogDebug("OptionalQuestionSignGrammarAction");
     return 0;
 }
 
 int EmptyOptionalGrammarAction() {
-    
+    LogDebug("EmptyOptionalGrammarAction");
     return 0;
 }
 
 int ParamsGrammarAction(const int param, const int params) {
-    
+    LogDebug("ParamsGrammarAction: param = %d, params = %d", param, params);
     return 0;
 }
 
 int ParamStringGrammarAction(const int sval) {
-    
+    LogDebug("ParamStringGrammarAction: sval = %d", sval);
     return 0;
 }
 
 int ParamIntegerGrammarAction(const int ival) {
-    
+    LogDebug("ParamIntegerGrammarAction: ival = %d", ival);
     return 0;
 }
 
 int RenderGrammarAction() {
-    
+    LogDebug("RenderGrammarAction");
     return 0;
 }
 
 int RenderAllGrammarAction() {
-    
+    LogDebug("RenderAllGrammarAction");
     return 0;
 }
 
 int MethodIdentifierGrammarAction(const int sval) {
-    
+    LogDebug("MethodIdentifierGrammarAction: sval = %d", sval);
     return 0;
 }
-
 /**
 * Esta acción se corresponde con el no-terminal que representa el símbolo
 * inicial de la gramática, y por ende, es el último en ser ejecutado, lo que
