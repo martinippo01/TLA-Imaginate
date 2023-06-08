b137dcb3d008a60d50158083b09c3f4275f876ca
martinippo01 <martinippo@gmail.com>
2023-06-08 15:59:33 -0300

    More python for code generation

--- src/backend/code-generation/python/generateImage.py
+++ src/backend/code-generation/python/generateImage.py
@@ -1,18 +1,30 @@
 from PIL import Image
 
-# Focos -> Un conjunto de posibles overlay images
-# Background image
-# Flavours -> Un conjunto de posibles overlay images
-# Filters -> Conjunto
-#           -
-#           -
-#           -
-#           -
 
-# Open the overlay image and convert it to RGBA mode
+# --------------------------------
+# Multiple focos and backgrounds |
+# -------------------------------
+focus_list = ["focus/Water.png", "focus/Plants.png", "focus/Stars.png", "focus/Swirl.png", "focus/Water.png"]
+bg_list = ["background/Bricks.png", "background/Canada.png", "background/Green.png", "background/Purple.png","background/Wood.png"];
+
+img_count = 0
+
+for bg in bg_list:
+    for focus in focus_list:
+        overlay_image = Image.open(focus).convert("RGBA")
+        background_image = Image.open(bg).convert("RGBA")
+        background_image.paste(overlay_image, (0,0), overlay_image)
+        background_image.save("exported-" + str(img_count) + ".png")
+        img_count = img_count + 1
+
+
+# ----------------------------
+# Single foco and background |
+# ---------------------------
 overlay_image = Image.open("focus/Water.png").convert("RGBA")
-background_image = Image.open("background/Bricks.png").convert("RGBA")
+background_image = Image.open("background/Wood.png").convert("RGBA")
 
 background_image.paste(overlay_image, (0, 0), overlay_image)
 
 background_image.show()
+background_image.save("exported.png")
