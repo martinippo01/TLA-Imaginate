44777e59a9ba06a87280f18eee2f363311b87762
Marco4444 <mscilipoti@itba.edu.ar>
2023-06-15 18:32:57 -0300

    added another example

--- test/accept/01-basic.py
+++
@@ -1,7 +0,0 @@
-Imaginate
-    .addFocus("./luna.png")
-    .addBackground("./darkblue.png")
-    .addFlavour("./nubes.png")
-    .addFlavour("./niebla.png")
-    .?addContrast(10, 20)
-    .render()
